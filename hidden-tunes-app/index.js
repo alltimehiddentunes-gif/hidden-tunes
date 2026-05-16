284b66bd1cd232c4cb794553579505ed87dbd24e
billy wills <mygermanlevel@gmail.com>
2026-05-16 20:46:11 +0200

    Set up native playback build foundation

--- hidden-tunes-app/index.js
+++ hidden-tunes-app/index.js
@@ -1,1 +1,22 @@
+const { USE_NATIVE_TRACK_PLAYER } = require("./constants/playbackConfig");
+
+if (USE_NATIVE_TRACK_PLAYER) {
+  try {
+    const Constants = require("expo-constants").default;
+    const isExpoGo = Constants?.appOwnership === "expo";
+
+    if (!isExpoGo) {
+      const TrackPlayer = require("react-native-track-player").default;
+
+      TrackPlayer.registerPlaybackService(() =>
+        require("./services/playbackServiceRegistration").default
+      );
+    }
+  } catch (error) {
+    if (__DEV__) {
+      console.warn("Track Player service registration skipped:", error);
+    }
+  }
+}
+
 require("expo-router/entry");
